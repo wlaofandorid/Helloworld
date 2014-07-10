bfa353a07c6a498abd5de2f45795eda6f3e82123
wla588 <12301017@bjtu.edu.cn>
2014-07-10 20:08:52 +0800

    Revert "Update MainActivity.java"
    This reverts commit 9ef9ba756523df76e443567fb4c2148bb779ea2b.

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -6,10 +6,10 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuIddddddddddddddddddddddddddddddddddtem;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Buxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxild;
+import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
